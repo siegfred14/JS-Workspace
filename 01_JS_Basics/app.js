2ee3621daa33ac5ca834918523c7623ca7d60fc9
Siegfred Samson <siegfredsamson@gmail.com>
2021-01-03 09:37:29 +0100

    initial JS Basics

---
+++ 01_JS_Basics/app.js
@@ -0,0 +1,26 @@
+console.log("Welcome To JavaScript");
+
+// console messages
+
+//log
+console.log('I am a log message');
+
+//info
+console.info('I am an Information message');
+
+//warn
+console.warn('I am a warning message');
+
+//error
+console.error('i am an error message');
+
+//Table
+
+let student = {
+    name: 'rajan',
+    Age: 22,
+    course: 'CSC'
+};
+
+console.table(student)
+
