558d9e7397f891398b8501df682f843a40c15b9c
Siegfred Samson <siegfredsamson@gmail.com>
2021-01-03 09:58:32 +0100

    initial DataTypes

---
+++ 03_JS_Data_Types/app.js
@@ -0,0 +1,36 @@
+let employeeName = 'John';
+console.log(typeof employeeName)
+console.log(`Name: ${employeeName} Type: ${typeof employeeName}`)
+
+
+
+//String
+let employee_Name = 'Siegfred';
+console.log(`value: ${employee_Name} Type: ${typeof employee_Name}`);
+
+//Number
+let employeeAge = 30;
+console.log(`value: ${employeeAge} Type: ${typeof employeeAge}`);
+
+//boolean
+let isManager = true;
+console.log(`value: ${isManager} Type: ${typeof isManager}`);
+
+//undefined
+let name; //variable declaration
+console.log(`value: ${name} Type: ${typeof name}`);
+name = 'Naveen'; // variable assignment
+
+// null (dummy value)
+let test = null;
+console.log(`value: ${test} Type: ${typeof test}`);
+
+//Arrays, Objects (future classes)
+
+//Re-Assignment is allowed
+
+let abc;
+abc = 10;
+abc = 'trial';
+console.log(`value: ${abc} Type: ${typeof abc}`);
+
