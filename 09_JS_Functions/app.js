e9abf20192a92cf0cbb9791250c38dd8aacef827
Siegfred Samson <siegfredsamson@gmail.com>
2021-01-06 11:58:07 +0100

    initial functions

---
+++ 09_JS_Functions/app.js
@@ -0,0 +1,10 @@
+// print from 0 to 10 diff 1
+
+let result = '';
+for (let i = 0; i <= 10; i++) {
+    result += `${i} `;
+}
+console.log(result);
+
+//using function for reusability of the above
+
