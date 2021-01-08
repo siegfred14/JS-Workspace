9314789e32d629874304610f49d900cabea3d6c4
Siegfred Samson <siegfredsamson@gmail.com>
2021-01-08 09:58:32 +0100

    added to Math Object

---
+++ 10_JS_Math_Object/app.js
@@ -0,0 +1,17 @@
+//sqrt
+let squareroot = Math.sqrt(144);
+console.log(`The Squareroot of 144 is : ${squareroot}`);
+
+//pow
+let power = Math.pow(2, 6);
+console.log(power);
+
+//min
+// let numbers = [12, 43, 56, 64, 34, 28, 18, 86, 40, 45];
+let minimum = Math.min(12, 43, 56, 64, 34, 28, 18, 86, 40, 45);
+console.log(minimum);
+
+//max
+let maximum = Math.max(12, 43, 56, 64, 34, 28, 18, 86, 40, 45);
+console.log(maximum);
+
