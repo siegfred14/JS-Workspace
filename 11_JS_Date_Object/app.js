a4a4aadbd7610746f2abae696defa5c61f9ce232
Siegfred Samson <siegfredsamson@gmail.com>
2021-01-08 10:27:41 +0100

    added to Date Object

--- 11_JS_Date_Object/app.js
+++ 11_JS_Date_Object/app.js
@@ -9,3 +9,17 @@ console.log(currentDate);
 let currentTime = new Date().toLocaleTimeString();
 console.log(currentTime);
 
+//Another country date,say USA Time
+let options = { timeZone: 'America/New_York' };
+let usaDate = new Date().toLocaleDateString('en-US', options);
+console.log(usaDate);
+
+//USA Time
+options = { timeZone: 'America/New_York' };
+let usaTime = new Date().toLocaleTimeString('en-US', options);
+console.log(usaTime);
+
+//today
+let today = new Date().getDay();
+console.log(today); // this outputs 4 since javascript uses numbers 0-6 from sunday for days.
+
