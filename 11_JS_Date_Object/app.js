66ac4f612a71b34f1d1b08ab3633a0cb08cb69ac
Siegfred Samson <siegfredsamson@gmail.com>
2021-01-08 11:11:01 +0100

    added to Date Object

--- 11_JS_Date_Object/app.js
+++ 11_JS_Date_Object/app.js
@@ -23,3 +23,44 @@ console.log(usaTime);
 let today = new Date().getDay();
 console.log(today); // this outputs 4 since javascript uses numbers 0-6 from sunday for days.
 
+//to get actual day
+let day = new Date().getDay();
+today = '';
+switch (day) {
+    case 0:
+        today = 'Sunday';
+        break;
+
+    case 1:
+        today = 'Monday';
+        break;
+
+    case 2:
+        today = 'Tuesday';
+        break;
+
+    case 3:
+        today = 'Wednesday';
+        break;
+
+    case 4:
+        today = 'Thursday';
+        break;
+
+    case 5:
+        today = 'Friday';
+        break;
+
+    case 6:
+        today = 'Saturday';
+        break;
+
+
+    default:
+        today = 'Enter a Proper Day';
+        break;
+
+}
+
+console.log(`Today is ${today}`);
+
