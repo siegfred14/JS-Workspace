477d84c6fbd716ea1e685b60d9556bd28620ef7f
Siegfred Samson <siegfredsamson@gmail.com>
2021-01-08 10:24:30 +0100

    added to Date Object

---
+++ 11_JS_Date_Object/app.js
@@ -0,0 +1,11 @@
+let date = new Date();
+console.log(date);
+
+//proper date
+let currentDate = new Date().toLocaleDateString();
+console.log(currentDate);
+
+//proper time
+let currentTime = new Date().toLocaleTimeString();
+console.log(currentTime);
+
