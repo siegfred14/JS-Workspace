22a696df98ca72e835daf3eb6a6c096b22a6870f
Siegfred Samson <siegfredsamson@gmail.com>
2021-01-09 10:05:26 +0100

    Number Operations Added

---
+++ 12_JS_Number_String_Objects/app.js
@@ -0,0 +1,14 @@
+//Numbers (operations on numbers)
+
+//max Number in JS
+console.log(Number.MAX_SAFE_INTEGER);
+
+//min Number in JS
+console.log(Number.MIN_SAFE_INTEGER);
+
+//Positive Infinity
+console.log(Number.POSITIVE_INFINITY);
+
+//Negative Ininity
+console.log(Number.NEGATIVE_INFINITY);
+
