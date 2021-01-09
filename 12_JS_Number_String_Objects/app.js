0a10d4a488a08f38cde4a9cff5975c751eeafef5
Siegfred Samson <siegfredsamson@gmail.com>
2021-01-09 10:40:22 +0100

    String Operations Added

--- 12_JS_Number_String_Objects/app.js
+++ 12_JS_Number_String_Objects/app.js
@@ -32,3 +32,50 @@ console.log(`${typeof num3} => ${typeof numString}`);
 let amount = 520;
 console.log(amount.toFixed(2)); //this fixes amount to two fractional positions
 
+//STRING RELATED OPERATIONS
+let msg = String('Good Morning');
+
+//to get String length
+console.log(msg.length);
+
+//to convert to lowercase
+console.log(msg.toLowerCase());
+
+//to convert to uppercase
+console.log(msg.toUpperCase());
+
+//Substring - substr(start index, no of chars)
+console.log(msg.substr(0, 4));
+
+console.log(msg.substr(5, 7));
+
+console.log(msg.substr(5));
+
+//charAt - to get the character at a given index 
+console.log(msg.charAt(5));
+
+//charCodeAt - to get the char ASCII code at a given Number
+console.log(msg.charCodeAt(5));
+
+
+//JSON - used to convert an object to srtring and string to object format
+/* 
+String to Object - parse()
+Object to String - stringify()
+*/
+
+let student = {
+    name: 'Rajan',
+    age: 20,
+    course: 'CSE',
+    college: 'IIT mumbai'
+};
+console.log(student);
+
+//to convert Object to string
+let studentstr = JSON.stringify(student);
+console.log(studentstr);
+
+//String to Object
+let newStudent = JSON.parse(studentstr);
+console.log(newStudent);
