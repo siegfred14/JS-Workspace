864c0b5e005bb7719a70bc5ff830b196d9c68f04
Siegfred Samson <siegfredsamson@gmail.com>
2021-01-11 10:30:04 +0100

    DOM Intro modified

---
+++ 14_JS_DOM_Intro/app.js
@@ -0,0 +1,37 @@
+//Document
+console.log(document);
+
+//title
+console.log(document.title);
+
+//body
+console.log(document.body);
+
+//header
+console.log(document.head);
+
+//nav
+let navTag = document.querySelector('nav');
+console.log(navTag);
+
+//anchor Tag
+let anchorTag = document.querySelector('nav a');
+console.log(anchorTag);
+
+//inner text of anchorTag
+let theText = anchorTag.innerText;
+console.log(theText);
+
+//change the inner text
+anchorTag.innerText = 'Siegfred Samson'
+
+//To apply CSS
+let h1Tag = document.querySelector('#msg');
+h1Tag.innerText = 'Good Evening';
+
+h1Tag.style.backgroundColor = 'limegreen';
+h1Tag.style.padding = '10px';
+h1Tag.style.color = 'white';
+h1Tag.style.textAlign = 'center';
+h1Tag.style.boxShadow = '0 0 10px black';
+h1Tag.style.borderRadius = '10px';
