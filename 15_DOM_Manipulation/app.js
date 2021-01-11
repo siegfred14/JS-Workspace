60a958d205a53d6cba1537f11c1aa9c86aba4d72
Siegfred Samson <siegfredsamson@gmail.com>
2021-01-11 10:59:14 +0100

    completed DOM Manipulation app.js

--- 15_DOM_Manipulation/app.js
+++ 15_DOM_Manipulation/app.js
@@ -1,3 +1,7 @@
 let greet = (message, color) => {
-    
+    let h1Tag = document.querySelector('#msg-1');
+    h1Tag.innerText = message;
+    h1Tag.style.backgroundColor = color;
+    h1Tag.style.padding = '15px';
+    h1Tag.style.boxShadow = '0 0 15px black';
 }
