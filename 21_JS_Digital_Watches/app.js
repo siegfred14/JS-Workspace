60fdb7a3adb6a265b41a7309abcce8bb6f3cdaa1
Siegfred Samson <siegfredsamson@gmail.com>
2021-01-18 10:00:24 +0100

    Shanghai time added

--- 21_JS_Digital_Watches/app.js
+++ 21_JS_Digital_Watches/app.js
@@ -18,3 +18,12 @@ let usaWatch = () => {
 };
 setInterval(usaWatch, 1000); //this means call the function every 1000 milisecond (1min);
 
+//china Time
+let chinaWatch = () => {
+    let options = { timeZone: 'Asia/Shanghai' };
+    let currentDate = new Date().toLocaleDateString('en-US', options);
+    let currentTime = new Date().toLocaleTimeString('en-US', options);
+    document.querySelector('#china-date').innerText = currentDate;
+    document.querySelector('#china-time').innerText = currentTime;
+};
+setInterval(chinaWatch, 1000); //this means call the function every 1000 milisecond (1min);
