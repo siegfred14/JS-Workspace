517b47d87342e6cf6b3fcdf7f136d3ced44b56fb
Siegfred Samson <siegfredsamson@gmail.com>
2021-01-18 09:52:06 +0100

    US time added

--- 21_JS_Digital_Watches/app.js
+++ 21_JS_Digital_Watches/app.js
@@ -8,3 +8,13 @@ let nigeriaWatch = () => {
 };
 setInterval(nigeriaWatch, 1000); //this means call the function every 1000 milisecond (1min);
 
+//USA Time
+let usaWatch = () => {
+    let options = { timeZone: 'America/New_York' };
+    let currentDate = new Date().toLocaleDateString('en-US', options);
+    let currentTime = new Date().toLocaleTimeString('en-US', options);
+    document.querySelector('#usa-date').innerText = currentDate;
+    document.querySelector('#usa-time').innerText = currentTime;
+};
+setInterval(usaWatch, 1000); //this means call the function every 1000 milisecond (1min);
+
