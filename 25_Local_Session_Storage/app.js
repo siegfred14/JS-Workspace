637054835396767a612b1411196e509de74ac460
Siegfred Samson <siegfredsamson@gmail.com>
2021-01-25 20:33:58 +0100

    Notes added

---
+++ 25_Local_Session_Storage/app.js
@@ -0,0 +1,15 @@
+/* 
+Storage
+-------
+-> localStorage -> saves only string -> JSON.stringify(), parse()
+    -> setItem('key', 'value'); //create/update
+    ->getItem('key'); //read
+    -> removeItem('key'); //delete
+    -> clear();
+
+-> sessionStorage ->
+    -> setItem('key', 'value'); //create/update
+    ->getItem('key'); //read
+    -> removeItem('key'); //delete
+    -> clear();
+ */
