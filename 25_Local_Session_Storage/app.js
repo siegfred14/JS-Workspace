2b92d7b089d4a440644cf407fb3cfaf8ab626c8f
Siegfred Samson <siegfredsamson@gmail.com>
2021-01-25 20:41:39 +0100

    storage operations

--- 25_Local_Session_Storage/app.js
+++ 25_Local_Session_Storage/app.js
@@ -13,3 +13,17 @@ Storage
     -> removeItem('key'); //delete
     -> clear();
  */
+
+let empName = 'John';
+
+//set
+localStorage.setItem('name', empName);
+
+//get
+console.log(localStorage.getItem('name'));
+
+//delete the key
+localStorage.removeItem('name');
+
+//clear
+localStorage.clear(); //this clears every key valued pair
