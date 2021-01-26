0f56d0d5dc9219bd3229f4ab10b814444fad6470
Siegfred Samson <siegfredsamson@gmail.com>
2021-01-26 10:25:35 +0100

    Local storage with an array added

--- 25_Local_Session_Storage/app.js
+++ 25_Local_Session_Storage/app.js
@@ -27,3 +27,17 @@ localStorage.removeItem('name');
 
 //clear
 localStorage.clear(); //this clears every key valued pair
+
+//Local storage with an array
+let colors = [{ id: 1, name: 'white' },
+{ id: 2, name: 'black' },
+{ id: 3, name: 'blue' },
+{ id: 4, name: 'yellow' }];
+
+localStorage.setItem("colorStore", JSON.stringify(colors)); //objects must be converted to string before storage
+
+let theColors = JSON.parse(localStorage.getItem('colorStore'));
+console.log(theColors);
+
+localStorage.removeItem('colorStore');
+
