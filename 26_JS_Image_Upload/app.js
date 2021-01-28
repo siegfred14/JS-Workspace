d8d6d4cc270c14f04f5e64652a07c16d696b0dcd
Siegfred Samson <siegfredsamson@gmail.com>
2021-01-28 20:11:00 +0100

    reading the image file

--- 26_JS_Image_Upload/app.js
+++ 26_JS_Image_Upload/app.js
@@ -5,5 +5,9 @@ uploadForm.addEventListener('submit', function () {
     let imageFile = document.querySelector('#customFile').files[0]; //file content
     let imageName = imageFile.name; //name on the file
 
+    //to read the image file
+    let reader = new FileReader();
+    reader.readAsDataURL(imageFile); //takes the imageFile an reads it as data 
+
     displayImage();
 });
