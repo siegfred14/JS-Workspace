5f47e69615e0f44cad07e9bdd00df3d594bac510
Siegfred Samson <siegfredsamson@gmail.com>
2021-01-28 20:09:53 +0100

    getting exact image file

--- 26_JS_Image_Upload/app.js
+++ 26_JS_Image_Upload/app.js
@@ -1,2 +1,9 @@
 //image upload
 let uploadForm = document.querySelector('#upload-form');
+uploadForm.addEventListener('submit', function () {
+    // To get the exact image file
+    let imageFile = document.querySelector('#customFile').files[0]; //file content
+    let imageName = imageFile.name; //name on the file
+
+    displayImage();
+});
