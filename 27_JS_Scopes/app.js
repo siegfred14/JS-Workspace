204008d903855ca8f1cd4d5138e4003e669912a5
Siegfred Samson <siegfredsamson@gmail.com>
2021-02-01 17:05:46 +0100

    statement scope modified

--- 27_JS_Scopes/app.js
+++ 27_JS_Scopes/app.js
@@ -1,14 +1,19 @@
 //JS BLOCK SCOPE 
 //using a statement scope/block
-let a = 10; //global scope/Parent scope
+let a = 10;
 {
-    let a = 20; //local scope/Child scope
+    let a = 20;
 }
-console.log(a); //Answer is 10
-
+console.log(a);
 
 {
     let a = 20;
     console.log(a); //answer is 20
 }
+
+let b = 15; //global scope/Parent scope
+{
+    let c = 23; //local scope/Child scope
+}
+console.log(b); //Answer is 15
 
