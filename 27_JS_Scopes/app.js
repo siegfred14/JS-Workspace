01c8e7de2cd4122956963f30a15f2df70d17427d
Siegfred Samson <siegfredsamson@gmail.com>
2021-02-01 17:14:37 +0100

    more on statement scopes

--- 27_JS_Scopes/app.js
+++ 27_JS_Scopes/app.js
@@ -19,3 +19,16 @@ console.log(b); //Answer is 15
 
 //NB you cannot redeclare the same variable in the same scope.
 //In this case, just call the variable without using let/const/var. eg a=12;
+
+//more on statement scopes
+a = 10;
+{
+    let a = 20;
+    {
+        let a = 30;
+        {
+            let a = 40;
+        }
+    } console.log(a);
+}
+
