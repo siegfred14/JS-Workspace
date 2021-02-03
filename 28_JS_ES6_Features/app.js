68d1bb31e1619253aceb5e356c34043f73d3e844
Siegfred Samson <siegfredsamson@gmail.com>
2021-02-03 17:30:09 +0100

    Example added

--- 28_JS_ES6_Features/app.js
+++ 28_JS_ES6_Features/app.js
@@ -1,2 +1,9 @@
 // 1. Template Strings (`) back tick operator
 //created to replace the string concantenation operator of Javascript
+
+let mobile = {
+    brand: 'Apple',
+    color: 'Silver',
+    price: 35000
+};
+
