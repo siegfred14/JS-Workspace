d475d71e5868ded564fa16b5316d6960b8bd29c7
Siegfred Samson <siegfredsamson@gmail.com>
2021-02-18 15:58:53 +0100

    default class modified

--- 29_JS_Classes/app.js
+++ 29_JS_Classes/app.js
@@ -1,10 +1,10 @@
 //Default object in Javascript
-let mobile ={
+/* let mobile ={
     brand : 'Apple',
     price: 35000,
     color: 'Silver'
 }; 
-console.log(mobile);
+console.log(mobile); */
 //The problem with this above object is that after contruction, we can still add a property and still delete a property
 //This does not represent a true life scenerio - you may alter the property value, but you cannot remove the vaue
 
@@ -15,4 +15,4 @@ class Mobile {
         this.color = color;
     }
 
-    
+}
