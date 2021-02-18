7a4b31a07cd40fac5fbd329d908ded854c2eba3c
Siegfred Samson <siegfredsamson@gmail.com>
2021-02-18 15:49:52 +0100

    comment added

--- 29_JS_Classes/app.js
+++ 29_JS_Classes/app.js
@@ -5,5 +5,5 @@ let mobile ={
     color: 'Silver'
 }; 
 console.log(mobile);
-
-
+//The problem with this above object is that after contruction, we can still add a property and still delete a property
+//This does not represent a true life scenerio - you may alter the property value, but you cannot remove the vaue
