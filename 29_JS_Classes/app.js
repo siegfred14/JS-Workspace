3a25c4ed9f21794501d5d0c9053bbf0cead2eff1
Siegfred Samson <siegfredsamson@gmail.com>
2021-02-18 15:57:29 +0100

    default class created

--- 29_JS_Classes/app.js
+++ 29_JS_Classes/app.js
@@ -7,3 +7,12 @@ let mobile ={
 console.log(mobile);
 //The problem with this above object is that after contruction, we can still add a property and still delete a property
 //This does not represent a true life scenerio - you may alter the property value, but you cannot remove the vaue
+
+class Mobile {
+    constructor(brand, price, color) {
+        this.brand = brand;
+        this.price = price;
+        this.color = color;
+    }
+
+    
