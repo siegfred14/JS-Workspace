823788e46e381897cb8f219d956f9043a179f9ee
Siegfred Samson <siegfredsamson@gmail.com>
2021-02-20 17:42:55 +0100

    class Mobile commented

--- 29_JS_Classes/app.js
+++ 29_JS_Classes/app.js
@@ -7,7 +7,7 @@
 console.log(mobile); */
 //The problem with this above object is that after contruction, we can still add a property and still delete a property
 //This does not represent a true life scenerio - you may alter the property value, but you cannot remove the vaue
-
+/* 
 class Mobile {
     constructor(brand, price, color) {
         this.brand = brand;
@@ -36,3 +36,4 @@ class Mobile {
 }
 let mobile = new Mobile('Apple', 35000, 'Silver');
 mobile.printSpecification();
+ */
