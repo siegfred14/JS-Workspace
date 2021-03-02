536a88d3c06023b00e9dc217d6c4e8b970564fd5
Siegfred Samson <siegfredsamson@gmail.com>
2021-03-02 01:48:06 +0100

    constructor created in Persons.js

--- 31_JS_Modules/persons/Persons.js
+++ 31_JS_Modules/persons/Persons.js
@@ -1,3 +1,6 @@
 export class Person {                           //using export allows you to use the module in some other class or file
-
+    constructor(firstName, lastName) {
+       
+    }
+    
 }
