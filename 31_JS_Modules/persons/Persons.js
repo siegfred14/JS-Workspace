0a4f4e8841a53abcea861809cabb3e733789c5b3
Siegfred Samson <siegfredsamson@gmail.com>
2021-03-02 01:48:59 +0100

    this keywords added in Persons.js

--- 31_JS_Modules/persons/Persons.js
+++ 31_JS_Modules/persons/Persons.js
@@ -1,6 +1,7 @@
 export class Person {                           //using export allows you to use the module in some other class or file
     constructor(firstName, lastName) {
-       
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
     
 }
