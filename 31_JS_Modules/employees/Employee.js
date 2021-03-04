4478494c8ff10ab374afbae1f36f12ff1a9f8cdb
Siegfred Samson <siegfredsamson@gmail.com>
2021-03-04 23:49:36 +0100

    getter getDesignation added in Employees.js

--- 31_JS_Modules/employees/Employee.js
+++ 31_JS_Modules/employees/Employee.js
@@ -1,4 +1,6 @@
-class Employee extends Person {
+import { Person } from '../persons/Persons.js'  //this allow you to 'extend Person'
+
+export class Employee extends Person {
     constructor(firstName, lastName, age, designation) {
         super(firstName, lastName);
         this.age = age;
@@ -7,6 +9,9 @@ class Employee extends Person {
     getAge() {
         return this.age;
     }
-   
+    getDesignation() {
+        return this.designation;
+    }
     
 }
+
