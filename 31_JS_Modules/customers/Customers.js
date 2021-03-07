3e95af33c0db412e155a84544c3941d3938eaaa7
Siegfred Samson <siegfredsamson@gmail.com>
2021-03-07 10:51:35 +0100

    super and this added

--- 31_JS_Modules/customers/Customers.js
+++ 31_JS_Modules/customers/Customers.js
@@ -2,7 +2,10 @@ import { Person } from '../persons/Persons.js'
 
 class Customer extends Person {
     constructor(firstName, lastName, age, location) {
-
+        super(firstName, lastName);
+        this.age = age;
+        this.location = location;
+        
     }
 
 };
