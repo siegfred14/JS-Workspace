8aac2f99eec012250cc86aedbd08158da4dbbede
Siegfred Samson <siegfredsamson@gmail.com>
2021-03-09 20:02:08 +0100

    Employee object called

--- 31_JS_Modules/app.js
+++ 31_JS_Modules/app.js
@@ -2,3 +2,5 @@ import { Employee } from './employees/Employee.js';
 import { Customer } from './customers/Customers.js'
 
 let employee = new Employee('John', 'Smith', 40, 'Senior Manager');
+employee.greet();
+
