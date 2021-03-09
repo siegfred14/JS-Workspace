f2b66f48008b4cad2fe8e143928c669c806a40e4
Siegfred Samson <siegfredsamson@gmail.com>
2021-03-09 20:02:38 +0100

    new Customer object created

--- 31_JS_Modules/app.js
+++ 31_JS_Modules/app.js
@@ -4,3 +4,4 @@ import { Customer } from './customers/Customers.js'
 let employee = new Employee('John', 'Smith', 40, 'Senior Manager');
 employee.greet();
 
+let customer = new Customer('Laura', 'Wilson', 28, 'India');
