3b79a41a83b12244742723fc693ddf2a59cd3963
Siegfred Samson <siegfredsamson@gmail.com>
2021-03-13 20:51:08 +0100

    simple function called with setInterval

--- 32_JS_SetInterval_SetTimeout/app.js
+++ 32_JS_SetInterval_SetTimeout/app.js
@@ -1,6 +1,15 @@
-let greet = () => {
+/* let greet = () => {
     console.log('Hello Welcome to JS');
 }
 greet();
 greet();
 greet();
+ */
+
+//to do this a better way, we use the setInterval.
+//The above becomes
+let greet = () => {
+    console.log('Hello Welcome to JS');
+};
+setInterval(greet, 1000); //this calls the greet function every 1000miliseconds MS
+
