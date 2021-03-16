e1311b5a7898079f7acac5de2b7397ee6d4788c5
Siegfred Samson <siegfredsamson@gmail.com>
2021-03-16 20:12:07 +0100

    Synchronous JS illustrated in  app.js

---
+++ 33_JS_Async_Programming/app.js
@@ -0,0 +1,8 @@
+//Synchronous Programming
+console.log('car 1');
+console.log('car 2');
+console.log('car 3');
+console.log('car 4');
+console.log('car 5');
+//it logs on console as it is declared
+
