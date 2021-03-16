1b3aa158397ab723a852bd2207662f3585ec65f6
Siegfred Samson <siegfredsamson@gmail.com>
2021-03-16 20:19:46 +0100

    Asynchronous illustration explained

--- 33_JS_Async_Programming/app.js
+++ 33_JS_Async_Programming/app.js
@@ -17,3 +17,6 @@ setTimeout(() => {
 console.log('car 4');
 console.log('car 5');
 
+//In this case, cars 1, 2, 4, 5 are logged immediately, car 3 comes in 3 seconds later
+//so you can write your codes randomly, then reorder their execution using async programming
+
