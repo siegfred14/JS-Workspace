b40382b42886ebbde8c1e7e7fc1cfa3ef47b8f10
Siegfred Samson <siegfredsamson@gmail.com>
2021-03-16 20:19:03 +0100

    Asynchronous JS illustrated in  app.js

--- 33_JS_Async_Programming/app.js
+++ 33_JS_Async_Programming/app.js
@@ -6,3 +6,14 @@ console.log('car 4');
 console.log('car 5');
 //it logs on console as it is declared
 
+//Asynchronous Programming
+//Imaging car 3 breaks down
+
+console.log('car 1');
+console.log('car 2');
+setTimeout(() => {
+    console.log('car 3');
+}, 3000)
+console.log('car 4');
+console.log('car 5');
+
