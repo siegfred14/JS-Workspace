40fc4842eecebf48a88f03dd0fb593952e422d74
Siegfred Samson <siegfredsamson@gmail.com>
2021-05-13 19:54:23 +0100

    createEmployee function added to app.js

--- 37_Async_Await/app.js
+++ 37_Async_Await/app.js
@@ -1,3 +1,10 @@
 let employees = [{ id: 1, name: 'John', age: 30 },
 { id: 2, name: 'Rajan', age: 25 }];
 
+//create Employee
+let createEmployee = (employee) => {
+    return new Promise((resolve, reject) => {
+        
+    })
+};
+
