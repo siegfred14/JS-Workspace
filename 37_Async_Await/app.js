2f80e9c6986d30504107c138b8205fe95ffead6a
Siegfred Samson <siegfredsamson@gmail.com>
2021-05-14 23:56:15 +0100

    employeeRows defined

--- 37_Async_Await/app.js
+++ 37_Async_Await/app.js
@@ -21,6 +21,7 @@ let createEmployee = (employee) => {
 
 let getEmployees = () => {
     setTimeout(() => {
+        let employeeRows = '';
         
         document.querySelector('#table-body').innerHTML = employeeRows;
     }, 1000);
